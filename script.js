d3e85107bb1f7f3c06d1d5105bbe3af656211bb9
IKedmenec <ivan.kec86@gmail.com>
2024-05-27 18:07:36 +0200

    promjena3333

---
+++ script.js
@@ -0,0 +1,12 @@
+function login() {
+let usermail = document.getElementById("uname");
+let userpassword = document.getElementById("psw");
+
+console.log (`Try to login with ${"usermail"}/${"userpassword"}`);
+
+}
+
+function register(){
+    window.location = "register.html";
+}
+
